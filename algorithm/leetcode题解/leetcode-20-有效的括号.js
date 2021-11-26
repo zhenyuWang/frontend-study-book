6fa2ad1a6546d7818d9feaa2abfe7564922f2c69
zhenyuWang <13641039885@163.com>
2021-11-27 01:12:30 +0800

    refactor: leetcode-20-有效的括号代码优化

--- algorithm/leetcode题解/leetcode-20-有效的括号.js
+++ algorithm/leetcode题解/leetcode-20-有效的括号.js
@@ -1,27 +1,30 @@
-/*
-  利用栈存储左括号，当碰到右括号的时候，判断栈顶值是否是与之匹配的左括号，如果是，弹出栈顶元素，否则返回false
-  最后如果栈为空，则证明没有多余的左括号，返回true，否则返回false
-*/
 var isValid = function(s) {
-  const stack = [],
-  obj = {
+    // 创建栈存储左括号
+    const stack = [],
+    // 利用obj维护对应关系
+    obj = {
       ')':'(',
       ']':'[',
       '}':'{'
-  }
-  for(let i = 0;i<s.length;i++){
+    }
+    // 遍历字符串
+    for(let i = 0;i<s.length;i++){
       switch(s[i]){
-          case '(':
-          case '[':
-          case '{':
-              stack.push(s[i])
-          break;
-          case ')':
-          case ']':
-          case '}':
-              if(stack.length === 0 || stack.pop() !== obj[s[i]]) return false;
-          break;
+        // 如果为左括号，入栈
+        case '(':
+        case '[':
+        case '{':
+          stack.push(s[i])
+        break;
+        // 如果为右括号，判断当前栈顶字符是否是其对应左括号，如果不是，返回 false
+        case ')':
+        case ']':
+        case '}':
+          if(stack.length === 0 || stack.pop() !== obj[s[i]]) return false;
+        break;
       }
-  }
-  return stack.length===0
-};
+    }
+    // 代码来到这里遍历过程中所有右括号都匹配到了对应的左括号
+    // 此时如果栈为不为空，说明左括号存在多余情况，反之说明输入字符串有效
+    return stack.length === 0
+  };
