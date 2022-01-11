c3fdf01148da6c1602da70b2526971a1ee96d905
zhenyuWang <13641039885@163.com>
2022-01-11 13:02:24 +0800

    feat: leetcode-1508-子数组和排序后的区间和

--- algorithm/leetcode-solution/leetcode-1508-子数组和排序后的区间和.js
+++ algorithm/leetcode-solution/leetcode-1508-子数组和排序后的区间和.js
@@ -28,60 +28,66 @@ var rangeSum = function(nums, n, left, right) {
   const heap = new Heap()
   // 首先将每一路的第一个值放入堆中
   for(let i = 0; i < n; i++) heap.push({i,j:i,sum:nums[i]})
+  // 初始化结果值
   let res = 0
+  // 从 1-right 遍历，获取前 right 个子数组和值
   for(let i = 1; i <= right; i++) {
+    // 获取当前未处理的子数组和值的最小值
     const min = heap.pop()
+    // 如果当前处于 left-right 区间，则累加结果值
     if(i >= left) res += min.sum
+    // 根据当前元素，推导所在路的下一个元素，插入堆中
     if(min.j<n-1) heap.push({i:min.i, j:min.j + 1, sum:min.sum + nums[min.j + 1]})
   }
+  // 返回结果
   return res%1000000007
 };
 // 小顶堆
 class Heap {
   constructor() {
-  // 初始化数组和size
-  this.arr = [];
-  this.size = 0;
-}
-// 插入操作
-push(item) {
-  this.arr.push(item);
-  this.size++;
-  // 插入后的自下向上平衡调整
-  if(this.size>1){
-    let cur = this.size-1,
-    parent = (cur-1)>>1;
-    while(cur>0 && this.arr[parent].sum>this.arr[cur].sum){
-      [this.arr[parent],this.arr[cur]] = [this.arr[cur],this.arr[parent]]
-      cur = parent,parent = (cur-1)>>1;
+    // 初始化数组和size
+    this.arr = [];
+    this.size = 0;
+  }
+  // 插入操作
+  push(item) {
+    this.arr.push(item);
+    this.size++;
+    // 插入后的自下向上平衡调整
+    if(this.size>1){
+      let cur = this.size-1,
+      parent = (cur-1)>>1;
+      while(cur>0 && this.arr[parent].sum>this.arr[cur].sum){
+        [this.arr[parent],this.arr[cur]] = [this.arr[cur],this.arr[parent]]
+        cur = parent,parent = (cur-1)>>1;
+      }
     }
   }
-}
-// 弹出操作
-pop() {
-  // 特殊处理弹出最后一个元素
-  if(this.arr.length===1){
+  // 弹出操作
+  pop() {
+    // 特殊处理弹出最后一个元素
+    if(this.arr.length===1){
+      this.size--;
+      return this.arr.pop()
+    }
+    const res = this.arr[0];
+    this.arr[0] = this.arr.pop();
     this.size--;
-    return this.arr.pop()
-  }
-  const res = this.arr[0];
-  this.arr[0] = this.arr.pop();
-  this.size--;
-  // 弹出后的自上向下平衡调整
-  let cur = 0,childl = 1,childr = 2;
-  while(
-    (childl<this.size && this.arr[childl].sum<this.arr[cur].sum) ||
-    (childr<this.size && this.arr[childr].sum<this.arr[cur].sum)
-  ){
-    if(childr<this.size && this.arr[childr].sum<this.arr[childl].sum){
-      [this.arr[cur],this.arr[childr]] = [this.arr[childr],this.arr[cur]]
-      cur = childr
-    }else{
-      [this.arr[cur],this.arr[childl]] = [this.arr[childl],this.arr[cur]]
-      cur = childl
+    // 弹出后的自上向下平衡调整
+    let cur = 0,childl = 1,childr = 2;
+    while(
+      (childl<this.size && this.arr[childl].sum<this.arr[cur].sum) ||
+      (childr<this.size && this.arr[childr].sum<this.arr[cur].sum)
+    ){
+      if(childr<this.size && this.arr[childr].sum<this.arr[childl].sum){
+        [this.arr[cur],this.arr[childr]] = [this.arr[childr],this.arr[cur]]
+        cur = childr
+      }else{
+        [this.arr[cur],this.arr[childl]] = [this.arr[childl],this.arr[cur]]
+        cur = childl
+      }
+      childl = cur*2+1,childr = cur*2+2;
     }
-    childl = cur*2+1,childr = cur*2+2;
+    return res;
   }
-  return res;
-}
 }
