4c3b9be1708c86bc0734baa67be1d045fd117bb7
zhenyuWang <13641039885@163.com>
2022-04-16 11:28:03 +0800

    feat: hand write selectSort

---
+++ JavaScript/hand-written-code/sort/__test__/shellSort.spec.ts
@@ -0,0 +1,10 @@
+  import shellSort from '../shellSort'
+
+  test('shellSort',() => {
+    const original = [5, 2, 6, 3, 8, -24, 43, 0, 1, 9, 7, -1, 4]
+    const expected = [-24, -1 ,0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 43]
+
+    shellSort(original)
+
+    expect(original).toStrictEqual(expected)
+  })
