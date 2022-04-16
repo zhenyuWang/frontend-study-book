4e03088b9103d40f7848cda24efd0686be0d28b0
zhenyuWang <13641039885@163.com>
2022-04-16 11:26:45 +0800

    feat: hand write countSort

---
+++ JavaScript/hand-written-code/sort/countSort.ts
@@ -0,0 +1,27 @@
+// O(n)/O(1)
+export default function countSort(original:number[]){
+  const len = original.length
+  const min = Math.min(...original)
+  const counts:number[] = []
+
+
+  for(let i = 0;i<len;i++){
+    const item = original[i]
+    const target = item-min
+
+    if(counts[target]!==undefined){
+      counts[target]++
+    }else{
+      counts[target] = 1
+    }
+  }
+
+  original.length = 0
+
+  for(let i = 0;i<counts.length;i++){
+    while(counts[i]){
+      original.push(i+min)
+      counts[i]--
+    }
+  }
+}
