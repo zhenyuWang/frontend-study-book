00d8c2ea6069df22a96e7c77f6057e631d06ea23
zhenyuWang <13641039885@163.com>
2022-04-16 11:27:01 +0800

    feat: hand write heapSort

---
+++ JavaScript/hand-written-code/sort/__test__/heapSort.spec.ts
@@ -0,0 +1,10 @@
+  import heapSort from '../heapSort'
+
+  test('heapSort',() => {
+    const original = [5, 2, 6, 3, 8, -24, 43, 0, 1, 9, 7, -1, 4]
+    const expected = [-24, -1 ,0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 43]
+
+    heapSort(original)
+
+    expect(original).toStrictEqual(expected)
+  })
---
+++ JavaScript/hand-written-code/sort/heapSort.ts
@@ -0,0 +1,77 @@
+// O(n*logn)/O(n)
+
+class MinHeap {
+  private list:number[] = []
+  public size:number = 0
+
+  push(val:number){
+    this.list.push(val)
+    this.size++
+
+    if(this.size>1){
+      let child = this.size-1
+      let parent = (child-1)>>1
+
+      while(child>0 && this.list[child]<this.list[parent]){
+        [this.list[parent],this.list[child]] = [this.list[child],this.list[parent]]
+
+        child = parent
+        parent = (child-1)>>1
+      }
+    }
+  }
+
+  pop(){
+    if(this.size===0){
+      return
+    }
+
+    if(this.size===1){
+      const res = this.list[0]
+      this.list = []
+      this.size = 0
+      return res
+    }
+
+    const res = this.list[0]
+    this.list[0] = this.list.pop()!
+    this.size--
+
+    let parent = 0
+    let childL = 1
+    let childR = 2
+
+    while(
+      (childL<this.size && this.list[childL]<this.list[parent])
+      || (childR<this.size && this.list[childR]<this.list[parent])
+    ){
+      if(childR<this.size && this.list[childR]<this.list[childL]){
+        [this.list[parent],this.list[childR]] = [this.list[childR],this.list[parent]]
+        parent = childR
+      }else{
+        [this.list[parent],this.list[childL]] = [this.list[childL],this.list[parent]]
+        parent = childL
+      }
+
+      childL = parent * 2 + 1
+      childR = parent * 2 + 2
+    }
+
+    return res
+  }
+}
+
+export default function heapSort(original:number[]){
+  const len = original.length
+  const minHeap = new MinHeap()
+
+  for(let i = 0;i<len;i++){
+    minHeap.push(original[i])
+  }
+
+  original.length = 0
+
+  while(minHeap.size){
+    original.push(minHeap.pop()!)
+  }
+}
