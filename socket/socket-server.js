e57eee5eaa6afcd58be7ec4b8aaa161ce5448d64
zhenyuWang <13641039885@163.com>
2023-06-19 18:54:58 +0800

    feat: socket demo

---
+++ socket/socket-server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.send('Hello, client! from server');
+  
+  socket.on('message', (data) => {
+    console.log(`Received message from client: ${data}`);
+    socket.send('this is server')
+  })
+  
+  socket.on('error', (err) => {
+    console.log('ws on error', err)
+  })
+  
+  socket.on('close', (err) => {
+    console.log('disconnected', err);
+  });
+});
