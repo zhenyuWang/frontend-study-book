90ed4d318ca05b8ee0860db9138c3cf62d5a565c
zhenyuWang <13641039885@163.com>
2023-07-04 10:35:55 +0800

    feat(react): next-blog data layout css

---
+++ React/next/nextjs-blog/components/message.js
@@ -0,0 +1,16 @@
+
+import styles from './message.module.css';
+import { clsx } from 'clsx';
+
+export default function Message({ children, type }) {
+  return (
+    <div
+      className={clsx({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error',
+      })}
+    >
+      {children}
+    </div>
+  );
+}
